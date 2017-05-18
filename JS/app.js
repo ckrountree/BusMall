d237414212d76761eb47387cededdc4833b63a96
ckrountree <jmail0207@gmail.com>
2017-05-18 10:19:20 -0700

    localStorage continuance upon refresh

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,8 @@
 'use strict';
-console.log(Chart);
 
 // create an array for all Productss to be shown to Focus Group
 var allProducts = [];
-console.log('is this working');
+
 // var selectedIndices = [];
 var prevThree = [];
 
@@ -16,7 +15,6 @@ function Products(name, id, filepath) {
     this.votes = 0;
 
     allProducts.push(this);
-
 }
 
 //   create new objects with name, id, and filepath
@@ -51,44 +49,39 @@ function instantiateProducts() {
 // var imageDisplay = document.getElementById('imageDisplay');
 // var src = imageDisplay.getAttribute('src');
 
-// shows the value of image for the element with id="imageDisplay"
-// alert(image); 
-//   create functions for tracking Display & Voting while on the page
-//   only want to display 3 images at any given moment
-//   begin voting count at zero (0)
-
+// create functions for tracking Display & Voting while on the page
+// only want to display 3 images at any given moment
+// begin voting count at zero (0) and store data at point of last click
+//       so that clicks can be picked up again after refresh
 var tracker = {
     display1: document.getElementsByClassName('display1')[0],
     display2: document.getElementsByClassName('display2')[0],
     display3: document.getElementsByClassName('display3')[0],
     imageDisplay: document.getElementById('imageDisplay'),
     votes: 0,
+    // votes: JSON.parse (localStorage.getItem ('currentVote')) || 0,
 
-//   create a function to select my random image
+//  create a function to select my random image
     randomIndex: function (array) {
         return Math.floor(Math.random() * array.length);
     },
 
-    getIndices: function (array) {
-
 //  selectedIndices array is NaN, so use '.length' to create one
-//  declare avariable set to the first array to  ensure that same-images do not appear on next sequential page
+//  declare a variable set to the first array to  ensure that same-images do not appear on next sequential page
+    getIndices: function (array) {
         var selectedIndices = [];
         while (selectedIndices.length < 3) {
             var item = this.randomIndex(array);
-            console.log('while loop working', item);
 
-
-//   make sure that same image doesn't show up more than once in one round of display (-1 means image already used)
+//  make sure that same image doesn't show up more than once in one round of display (-1 means image already used)
             if (selectedIndices.indexOf(item) === -1 && prevThree.indexOf(item) === -1) {
-                console.log('inside of if block', item);
                 selectedIndices.push(item);
                 prevThree.push(item);
                 allProducts[ item ].timesShown ++;
             }
         }
 
-//   make sure that same image doesn't appear in the next sequential page
+//  make sure that same image doesn't appear in the next sequential page
         prevThree = selectedIndices;
         console.log('selected indices' + selectedIndices);
         console.log('previous 3' + prevThree);
@@ -96,9 +89,9 @@ var tracker = {
 
     },
 
+//  get 3 random product images
     displayOptions: function () {
         console.log('you\'re getting 3 random images');
-// get 3 random product images
         var randomProducts = this.getIndices(allProducts);
         var index1 = randomProducts[0];
         var index2 = randomProducts[1];
@@ -108,7 +101,7 @@ var tracker = {
         var products2 = allProducts[index2];
         var products3 = allProducts[index3];
 
-// append to the DOM
+//  append to the DOM
         this.display1.innerText = products1.name;
         this.display2.innerText = products2.name;
         this.display3.innerText = products3.name;
@@ -127,34 +120,29 @@ var tracker = {
 //  adding the number of votes (clicks) for each product
         allProducts.forEach(function runEach(product) {
             if (product.id === id) {
-                console.log(product.id + ' what is this doing');
                 product.votes += 1;
             }
         });
 
-        if (this.votes > 24) {
+        if (this.votes > 3) {
             this.showResults();
         }
     },
 
-//   show results after user selects (clicks) 25 items
+//  show results after user selects (clicks) 25 items
     showResults: function () {
         this.imageDisplay.removeEventListener('click', voteHandler);
         console.table(allProducts);
 
-//   push the data to table (number of times each product has been chosen by users)
+//  push the data to table (number of times each product has been chosen by users)
         var resultData = [];
         var labels = [];
         for (var j = 0; j < allProducts.length; j++) {
             resultData.push(allProducts[j].votes);
-            console.log(resultData + ' votes working');
-            labels.push(allProducts[j].id);
-            console.log(labels + ' id working');
         }
 
-//   this adds the chart of results, via Canvas
+//  this adds the chart of results, via Canvas
         var canvas = document.getElementById('resultsChart');
-            console.log(canvas);
         var itemChosen = new Chart (canvas, {
             type: 'bar',
             data: {
@@ -174,12 +162,10 @@ var tracker = {
                 maintainAspectRatio: true
             }
         })
-
     }
-
 }
 
-//   add an Event handler
+//  add an Event handler
 tracker.imageDisplay.addEventListener('click', voteHandler);
 function voteHandler() {
     if (event.target.id) {
@@ -188,19 +174,16 @@ function voteHandler() {
     }
 
 //  TODO store submissions in LocalStorage so I can refresh or close the page and not lose my results
-localStorage.setItem('voteData', JSON.stringify(allProducts));
+// localStorage.setItem( 'voteData', JSON.stringify(allProducts ));
+localStorage.setItem( 'currentVote', JSON.stringify(tracker.votes ));
 
 //  below is same expression, different method:
 // localStorage.voteData = allProducts
-
-
 }
 
-
 //  initialize app / call 
-
 instantiateProducts();
 tracker.displayOptions();
-console.log(tracker);
+
 
 
